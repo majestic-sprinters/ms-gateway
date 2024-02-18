2a2fc46feb8bb5fd2e5e82b83500d73fd884c1d4
Duman Marlambekov <74466653+Dumanbruh@users.noreply.github.com>
2024-02-18 21:52:33 +0600

    #3 Route set up; Configure CI/CD (#8)
    * fix: added ms-backend
    
    * #3 Set up CI/CD(#2)
    
    * Create Dockerfile script
    
    * Write action script for deployment
    
    * Add a checkout step for the action
    
    * Decrement checkout version
    
    * Set concrete machine to run on
    
    * Separate image build and container run to separate jobs
    
    * Set repository secret to env
    
    * Set docker image tag explicitly
    
    * Fix docker repository name
    
    * Add registry name
    
    * Fix input name to tags
    
    * Update deployment script: deploy on docker swarm node
    
    * Write test step
    
    * Return old steps
    
    * Mark stages for jobs
    
    * Remove invalid stage input
    
    * Update job label
    
    * Update docker target stack name
    
    * Update image pull policy to refresh specific one
    
    * Set the default main branch as trigger branch
    
    * Synchronize routing with API contract, add test route
    
    ---------
    
    Co-authored-by: Zhomartkhan Talgatuly <71120576+khanZhomart@users.noreply.github.com>
    Co-authored-by: khanZhomart <jtalgatulu@yandex.kz>

--- src/main/java/kz/azure/ms/config/GatewayConfig.java
+++ src/main/java/kz/azure/ms/config/GatewayConfig.java
@@ -8,15 +8,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-
-    //Programmatically creating a route
-    //Simple routes in the application.yml
-    //Flexible and complex routes in the customRouteLocator method
+    /**
+     * Programmatically creating a route
+     * - Simple routes in the application.yml
+     * - Complex routes in the customRouteLocator method
+     */
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(p -> p.path("/v1/**")
+                .route(p -> p.path("/test/**")
                         .uri("https://example.org"))
                 .build();
     }
+
 }
