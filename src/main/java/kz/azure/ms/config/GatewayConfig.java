4c00938e974e1c165de271695161697de06b80bc
DumanBruh <dmarlambekov@gmail.com>
2024-02-19 22:28:09 +0600

    #9 Route fix

--- src/main/java/kz/azure/ms/config/GatewayConfig.java
+++ src/main/java/kz/azure/ms/config/GatewayConfig.java
@@ -8,16 +8,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-
-    //Programmatically creating a route
-    //Simple routes in the application.yml
-    //Flexible and complex routes in the customRouteLocator method
+    /**
+     * Programmatically creating a route
+     * - Simple routes in the application.yml
+     * - Complex routes in the customRouteLocator method
+     */
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-//                .route("ms-backend-route", r -> r.path("/ms-backend/**")
-//                        .filters(f -> f.rewritePath("/ms-backend/(?<remaining>.*)", "/${remaining}"))
-//                        .uri("http://localhost:8080"))
+                .route(p -> p.path("/test/**")
+                        .uri("https://example.org"))
                 .build();
     }
 
